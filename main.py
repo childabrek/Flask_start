fc871deae6fa18024429e64471fbc1dc6b627f8d
chilldabrek <nik.abndreev.16@mail.ru>
2021-03-22 21:35:57 +0500

    main#1

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
 from flask import Flask
-from data import db_session
-from data.users import User
-from data.news import News
-from data.jobs import Jobs
 from flask import render_template
+from loginform import LoginForm
+from flask import redirect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-db_session.global_init("db/blogs.db")
 
 
-@app.route("/")
-def index():
-    db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.is_private != True)
-    return render_template("index.html", news=news)
+# app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-def main():
-    # app.run()
-    db_sess = db_session.create_session()
-
-    job = Jobs()
-    job.team_leader = 1
-    job.job = 'deployment of residential modules 1 and 2'
-    job.work_size = 15
-    job.collaborators = '2, 3'
-    job.is_finished = False
-    db_sess.add(job)
-    db_sess.commit()
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template("base.html", title=title)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
